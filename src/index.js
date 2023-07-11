e19e64d0449f3660ad8e30d97abfe6637ed3a1d2
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-07-10 17:03:28 -0700

    add slider functionality

--- src/index.js
+++ src/index.js
@@ -5,47 +5,99 @@ import img3 from './images/slide-img3.jpg';
 import img4 from './images/slide-img4.jpg';
 import img5 from './images/slide-img5.jpg';
 
-function createSlider (ImageArr) {
+function createSlider (ImageArr, 
+    {
+    currentSlideIndex = 0, 
+    duration = 750, 
+    easing = 'cubic-bezier(0.250, 0.460, 0.450, 0.940)',
+    slideShowInterval = 4000
+   } = {}
+   ) {
     const slider = document.createElement('div');
+    slider.className = 'slider';
+    slider.innerHTML = `
+    <div class="slides-wrapper"></div> 
+    <div class="slide-indicators"></div> 
+    <button type="button" class="prev-button"></button> 
+    <button type="button" class="next-button"></button>
+    `;
 
-    slider.innerHTML = 
+    const [wrapper, indicators, prevBtn, nextBtn] = slider.children;
+    
 
-    `<div class="slides-wrapper">
-    <div class="slide active" style="background: yellow;"></div>
-    <div class="slide" style="background: brown;"></div>
-    <div class="slide" style="background: purple;"></div>
-    <div class="slide" style="background: orange;"></div>
-    <div class="slide" style="background: red;"></div>
-  </div>
-  <div class="slide-indicators">
-    <button type="button" class="active"></button>
-    <button type="button"></button>
-    <button type="button"></button>
-    <button type="button"></button>
-    <button type="button"></button>
-  </div>
-  <button type="button" class="prev-button"></button>
-  <button type="button" class="next-button"></button>`
+    ImageArr.forEach((image, index) => {
+        const imageSlide = document.createElement('div');
+        imageSlide.classList.add('slide');
+        const indicatorBtn = document.createElement('button');
+            if (index === 0) {
+            imageSlide.classList.add('current-slide');
+            indicatorBtn.focus();
+        }
+        const img = new Image();
+        img.src = image;
+
+        wrapper.appendChild(imageSlide);
+        imageSlide.appendChild(img);
+        indicators.appendChild(indicatorBtn);
+    });
+
+    const slidesArr = [...wrapper.children];
+    const indicatorButtonsArr = [...indicators.children];
+    
+    function setCurrentSlide() {
+        slidesArr.forEach((slide, index) => {
+            if (index === currentSlideIndex) slide.classList.add('current-slide'); 
+            else slide.classList.remove('current-slide');
+        })
+    };
+
+    function changeIndex(button) {
+        
+        if (button === 'next') {
+            if (currentSlideIndex < slidesArr.length-1) currentSlideIndex += 1;
+            else currentSlideIndex = 0;
+        } else if (button === 'prev') {
+            if (currentSlideIndex <= slidesArr.length-1 && currentSlideIndex > 0) currentSlideIndex -= 1; 
+                else currentSlideIndex = slidesArr.length-1;         
+        }   
+    }
+
+    function changeIndicatorFocus() {
+        document.querySelector('.slide-indicators').children[currentSlideIndex].focus();
+    };
 
 
-    // const [wrapper, indicators, prevBtn, nextBtn] = slider.children;
+    nextBtn.addEventListener('click', () => {
+        // wrapper.children[currentSlideIndex].animate(
+        //     [{ transform: "translateX(0)" }, { transform: "translateX(100%)" }],
+        //     {
+        //       duration,
+        //       easing,
+        //       fill: 'forwards',
+        //     }
+        //   );
+        changeIndex('next');
+        setCurrentSlide(slidesArr);
+        changeIndicatorFocus();
 
-    // ImageArr.forEach(image => {
-    //     const imageSlide = document.createElement('div');
-    //     imageSlide.classList.add('slide');
-    //     const indicatorBtn = document.createElement('button');
-    //     const img = new Image();
-    //     img.src = image;
 
-    //     wrapper.appendChild(imageSlide);
-    //     imageSlide.appendChild(img);
-    //     indicators.appendChild(indicatorBtn);
-    // });
+
+    });
+
+    prevBtn.addEventListener('click', () => {
+        changeIndex('prev');
+        setCurrentSlide(slidesArr);
+        changeIndicatorFocus();
+    });
 
 
     return slider;
 };
 
 document.body.appendChild(createSlider([img1, img2, img3, img4, img5]))
+
+
+
+
 
 
